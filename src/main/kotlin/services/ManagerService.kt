1fdbf8d703b7dc868114416c5ed55983dac63e22
Douglas Holanda <douglasholanda3195@gmail.com>
2024-03-02 16:12:28 -0300

    Creation of first interaction with manager

--- src/main/kotlin/services/ManagerService.kt
+++ src/main/kotlin/services/ManagerService.kt
@@ -1,9 +1,34 @@
 package services
 
+import entities.persons.Manager
+import utilities.Util.Companion.printMessage
+
 class ManagerService {
     companion object{
-        fun doLoginManager(){
+        fun doLoginManager() {
+            val manager = Manager(1, "Douglas Holanda", "doug", "1609")
+            var attempts : Int = 3
+            printMessage("  LOGIN BANK MANAGER")
+            println("Enter your username and password:")
 
+            do {
+                println("Username : ")
+                var username = readLine()
+                println("Password : ")
+                var password = readLine()
+
+                if (manager.username.equals(username) && manager.password.equals(password)){
+                    println("Login successfully!")
+                    doInteractesManager(manager)
+                }
+                else{
+                    println("Credentials not recognized.\n ")
+                    attempts--
+                }
+            } while(attempts > 0)
+        }
+        fun doInteractesManager(manager: Manager){
+            printMessage("      Welcome to your page, dearest ${manager.nameManager}      \n")
         }
     }
 }
