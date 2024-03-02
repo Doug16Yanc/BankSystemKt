27d647c24458ddae1ad17f03560760a614607ac0
Douglas Holanda <douglasholanda3195@gmail.com>
2024-03-02 16:45:05 -0300

    Creation of first interaction with clerks

--- src/main/kotlin/services/ClerkService.kt
+++ src/main/kotlin/services/ClerkService.kt
@@ -1,9 +1,47 @@
 package services
 
+import entities.persons.Clerk
+import utilities.Util.Companion.printMessage
+
 class ClerkService {
     companion object{
         fun doLoginClerk(){
+            var clerks : MutableList<Clerk> = ArrayList<Clerk>()
+            val clerk1 = Clerk(2, "Needham", "need@gmail.com", "need", "20")
+            val clerk2 = Clerk(3, "Doberainer", "dobe@gmail.com", "dobe", "30")
+            clerks.add(clerk1)
+            clerks.add(clerk2)
+            var helpClerk = Clerk(1, "", "", "", "")
+            var login : Boolean = false
+            var attempts : Int = 3
+            printMessage("      PAGE LOGIN CLERK        \n")
+            println("Enter your username and password : ")
+            do {
+                println("Username : ")
+                var username = readLine()
+                println("Password : ")
+                var password = readLine()
 
+                for (clerk : Clerk in clerks){
+                    if (clerk.username.equals(username) && clerk.password.equals(password)){
+                        login = true
+                        helpClerk = clerk
+                        break
+                    }
+                }
+                if (login){
+                    println("Login successfully!\n")
+                    interactesClerk(helpClerk)
+                }
+                else{
+                    println("Credentials not recognized.\n")
+                    attempts--
+                }
+            } while (attempts > 0)
+
+        }
+        fun interactesClerk(clerk : Clerk){
+            printMessage("      Welcome to your page, dearest ${clerk.nameClerk}\n")
         }
     }
 }
--- src/main/kotlin/services/CustomerService.kt
+++ src/main/kotlin/services/CustomerService.kt
@@ -1,10 +1,47 @@
 package services
 
+import utilities.Util.Companion.printMessage
+import java.util.*
+
 class CustomerService {
     companion object {
 
         fun asksAboutCustomer() {
+            printMessage("Welcome to our page of customers\n")
+            println("Have you already registered in our system?\n   Y/y - Yes\n  N/n - Not\n")
+            var option = readLine()?.trim()?.lowercase(Locale.getDefault())
 
+            when(option){
+                "y" -> {
+                    doLoginCustomer()
+                }
+                "n" -> {
+                    recordCustomer()
+                }
+                else -> {
+                    printMessage("Sorry, however this option´s no existent.\n")
+                }
+            }
+        }
+        fun doLoginCustomer(){
+
+        }
+        fun recordCustomer(){
+            printMessage("Page of customer registers\n")
+            println("Are you legal or natural person?\n L/l - Legal \n N/n - Natural\n")
+            var option = readLine()?.trim()?.lowercase(Locale.getDefault())
+
+            when(option){
+                "l" -> {
+
+                }
+                "n" -> {
+
+                }
+                else -> {
+                    printMessage("Sorry, however this option´s no existent.\n")
+                }
+            }
         }
     }
 }
