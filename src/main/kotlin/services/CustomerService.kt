98977b04a27130c40fbdfccef26f29b1e13f576c
Douglas Holanda <douglasholanda3195@gmail.com>
2024-03-02 17:44:42 -0300

    Creation of login function for customers

--- src/main/kotlin/services/CustomerService.kt
+++ src/main/kotlin/services/CustomerService.kt
@@ -12,44 +12,131 @@ class CustomerService {
     companion object {
 
         fun asksAboutCustomer() {
+            var legals: MutableMap<Int, LegalCustomer> = HashMap<Int, LegalCustomer>()
+            var naturals : MutableMap<Int, NaturalCustomer> = HashMap<Int, NaturalCustomer>()
+
             printMessage("Welcome to our page of customers\n")
             println("Have you already registered in our system?\n   Y/y - Yes\n  N/n - Not\n")
             var option = readLine()?.trim()?.lowercase(Locale.getDefault())
 
             when(option){
                 "y" -> {
-                    doLoginCustomer()
+                    doLoginCustomer(legals, naturals)
                 }
                 "n" -> {
-                    recordCustomer()
+                    recordCustomer(legals, naturals)
                 }
                 else -> {
                     printMessage("Sorry, however this option´s no existent.\n")
                 }
             }
         }
-        fun doLoginCustomer(){
+        fun doLoginCustomer(legals: MutableMap<Int, LegalCustomer>, naturals: MutableMap<Int, NaturalCustomer>) {
+            printMessage("Page of customer login\n")
+            println("Who are you?\n L/l - Legal\n N/n - Natural\n")
+            var option = readLine()?.trim()?.lowercase(Locale.getDefault())
 
+            when(option){
+                "l" -> {
+                    doLoginLegal(legals)
+                }
+                "n" -> {
+                    doLoginNatural(naturals)
+                }
+                else -> {
+                    printMessage("Sorry, however this option´s no existent.\n")
+                }
+            }
         }
-        fun recordCustomer(){
+
+        fun doLoginLegal(legals: MutableMap<Int, LegalCustomer>) {
+            var attempts : Int = 0
+            var found : Boolean = false
+            printMessage("Page of login legal customer\n")
+
+            println("Enter your id:")
+            var id = sc.nextInt()
+
+            if (legals.containsKey(id)){
+                println("Enter your username and password:")
+                do{
+                    println("Username : ")
+                    var username = readLine()
+                    println("Password : ")
+                    var password = readLine()
+
+                    for (legal in legals.values){
+                        if (legal.username.equals(username) && legal.password.equals(password)){
+                            found = true
+                        }
+                    }
+                    if (found){
+                        println("Login successfully!\n")
+                    }
+                    else{
+                        println("Credentials not recognized!\n")
+                        attempts--
+                    }
+                } while(attempts > 0)
+
+            }
+            else {
+               println("Customer not found.\n")
+           }
+        }
+        fun doLoginNatural(naturals: MutableMap<Int, NaturalCustomer>) {
+            var attempts : Int = 0
+            var found : Boolean = false
+            printMessage("Page of login natural customer\n")
+
+            println("Enter your id:")
+            var id = sc.nextInt()
+
+            if (naturals.containsKey(id)){
+                println("Enter your username and password:")
+                do{
+                    println("Username : ")
+                    var username = readLine()
+                    println("Password : ")
+                    var password = readLine()
+
+                    for (natural in naturals.values){
+                        if (natural.username.equals(username) && natural.password.equals(password)){
+                            found = true
+                        }
+                    }
+                    if (found){
+                        println("Login successfully!\n")
+                    }
+                    else{
+                        println("Credentials not recognized!\n")
+                        attempts--
+                    }
+                } while(attempts > 0)
+
+            }
+            else {
+                println("Customer not found.\n")
+            }
+        }
+        fun recordCustomer(legals: MutableMap<Int, LegalCustomer>, naturals: MutableMap<Int, NaturalCustomer>) {
             printMessage("Page of customer registers\n")
             println("Are you legal or natural person?\n L/l - Legal \n N/n - Natural\n")
             var option = readLine()?.trim()?.lowercase(Locale.getDefault())
 
             when(option){
                 "l" -> {
-                    recordCustomerLegal()
+                    recordCustomerLegal(legals)
                 }
                 "n" -> {
-                    recordCustomerNatural()
+                    recordCustomerNatural(naturals)
                 }
                 else -> {
                     printMessage("Sorry, however this option´s no existent.\n")
                 }
             }
         }
-       fun recordCustomerLegal() {
-           var legals: MutableMap<Int, LegalCustomer> = HashMap<Int, LegalCustomer>()
+       fun recordCustomerLegal(legals: MutableMap<Int, LegalCustomer>) {
 
            var id = GenerationId.generateId(legals)
            println("Name : ")
@@ -74,8 +161,7 @@ class CustomerService {
            legals.put(id, legalCustomer)
 
         }
-        fun recordCustomerNatural() {
-            var naturals : MutableMap<Int, NaturalCustomer> = HashMap<Int, NaturalCustomer>()
+        fun recordCustomerNatural(naturals: MutableMap<Int, NaturalCustomer>) {
 
             var id = GenerationId.generateId(naturals)
             println("Name : ")
