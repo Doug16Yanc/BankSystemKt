967d2ebace5527deb73b0ae0e236d2d99a093e6c
Douglas Holanda <douglasholanda3195@gmail.com>
2024-03-02 15:04:13 -0300

    My first commit for this project

---
+++ src/main/kotlin/application/Program.kt
@@ -0,0 +1,8 @@
+package application
+
+import utilities.Util.Companion.printMessage
+
+fun main() {
+    printMessage("Welcome to bank!")
+    println("Who are you?")
+}
---
+++ src/main/kotlin/utilities/Util.kt
@@ -0,0 +1,11 @@
+package utilities
+
+class Util {
+    companion object{
+        fun printMessage(message : String){
+            println("************************************")
+            println(message)
+            println("************************************")
+        }
+    }
+}
